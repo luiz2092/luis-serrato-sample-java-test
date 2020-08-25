4824eb5b6d84ba954142742e7560405c9bb39ba2
Luis Serrato <luis.serrato@badak.mx>
2020-08-25 02:38:31 -0500

    valid empty root folder

--- src/main/java/com/simple/test/SimpleTestLuisSerratoApplication.java
+++ src/main/java/com/simple/test/SimpleTestLuisSerratoApplication.java
@@ -1,6 +1,8 @@
 package com.simple.test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +10,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.opencsv.CSVWriter;
 import com.simple.test.utils.Constants;
+import com.simple.test.utils.Utils;
 
 @SpringBootApplication
 public class SimpleTestLuisSerratoApplication {
@@ -23,9 +27,32 @@ public class SimpleTestLuisSerratoApplication {
 	}
 
 	@PostConstruct
-	public void createRoot() {
-		File userDir = new File(Constants.ROOT_PATH);
-		userDir.mkdirs();
+	public void createRoot() throws IOException {
+		File rootDir = new File(Constants.ROOT_PATH);
+		rootDir.mkdirs();
+
+		if (rootDir.listFiles().length == 0) {
+
+			File userDir = new File(Constants.ROOT_PATH.concat("1066"));
+			userDir.mkdirs();
+
+			for (int i = 0; i < 6; i++) {
+				File newTransaction = new File(Constants.ROOT_PATH.concat("1066"),
+						Utils.generateUuid().concat(Constants.CSV_EXTENSION));
+				newTransaction.getAbsoluteFile().createNewFile();
+
+				FileWriter output = new FileWriter(newTransaction);
+				CSVWriter writer = new CSVWriter(output, ',', CSVWriter.NO_QUOTE_CHARACTER,
+						CSVWriter.NO_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+
+				writer.writeNext(Constants.CSV_HEADERS);
+
+				writer.writeNext(new String[] { "1066", "120.30", "DesTransac", Constants.INIT_DATES[i] });
+				writer.close();
+
+			}
+
+		}
 	}
 
 }
--- src/main/java/com/simple/test/utils/Constants.java
+++ src/main/java/com/simple/test/utils/Constants.java
@@ -13,4 +13,8 @@ public class Constants {
 	public static final String CSV_EXTENSION = ".csv";
 
 	public static final String[] CSV_HEADERS = new String[] { "USER_ID", "AMOUNT", "DESCRIPTION", "DATE" };
+
+	public static final String[] INIT_DATES = new String[] { "2020-01-09", "2020-01-10", "2020-01-11", "2020-01-20",
+			"2020-01-22", "2020-01-30" };
+
 }
